f15eb9cca06a5a44f75c30e0c5750bbb2ca28607
Joiner Calderon <jcvalle_13@hotmail.com>
2020-08-10 22:40:44 -0600

    Creacion de capas

---
+++ AccesoDatos/Acceso.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AccesoDatos
+{
+    public class Acceso
+    {
+        #region Atributos
+        //Configuracion string de conexion BD Joiner Prestamos
+        private string strconexion = Properties.Settings.Default.Prestamos;
+
+        //AGREGAR string conexion Steph
+
+
+        private SqlConnection objconexion;
+        #endregion
+
+        #region Constructor
+
+        public Acceso()
+        {
+            try
+            {
+                //Inicializa la conexion con la cadena 
+                objconexion = new SqlConnection(strconexion);
+                this.ABRIRCONEXION();  //se abre conexión
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                this.CERRARCONEXION(); //Cierre de conexión
+            }
+        }
+        #endregion
+
+        #region Metodos privados
+
+        private void ABRIRCONEXION()
+        {
+            if (objconexion.State == System.Data.ConnectionState.Closed)
+                objconexion.Open();
+        }
+
+        private void CERRARCONEXION()
+        {
+            if (objconexion.State == System.Data.ConnectionState.Open)
+                objconexion.Close();
+        }
+
+        #endregion
+
+    }
+}
