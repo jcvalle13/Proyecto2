fb949211d4e57658ae32a9e0f8fcef9d4c3a2856
Joiner Calderon <jcvalle_13@hotmail.com>
2020-08-12 23:11:58 -0600

    nuevo

---
+++ Servicios/IServicios.cs
@@ -0,0 +1,51 @@
+﻿using Entidades;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+
+
+namespace Servicios
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IServicios" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServicios
+    {
+        #region Usuarios
+        [OperationContract]
+        int AgregarUsuario(Usuarios P_Usuario);
+
+        [OperationContract]
+        int ModificarUsuario(Usuarios P_Usuario);
+
+        [OperationContract]
+        List<Usuarios> Consultar_Usuarios();
+
+        [OperationContract]
+        bool VerificarUsuario(Usuarios P_usuario);
+
+        [OperationContract]
+        List<Usuarios> Consultar_Permisos_Usuarios(Usuarios P_usuario);
+
+        [OperationContract]
+        int EliminarUsuario(Usuarios P_usuario);
+
+        #endregion
+
+        #region Perfiles
+
+        [OperationContract]
+        int AgregarPerfil(Perfiles P_Perfil);
+
+        [OperationContract]
+        int ModificarPerfil(Perfiles P_Perfil);
+
+        [OperationContract]
+        int EliminarPerfil(Perfiles P_Perfil);
+
+
+        [OperationContract]
+        List<Perfiles> ConsultarPerfiles(Perfiles P_Perfil);
+        #endregion
+
+    }
+}
---
+++ Servicios/Servicios.svc.cs
@@ -0,0 +1,69 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using Negocio;
+
+
+
+namespace Servicios
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Servicios" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Servicios.svc o Servicios.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Servicios : IServicios
+    {
+        public int AgregarPerfil(Perfiles P_Perfil)
+        {
+            return LogicNegocio.AgregarPerfil(P_Perfil);
+        }
+
+        public int AgregarUsuario(Usuarios P_Usuario)
+        {
+            return LogicNegocio.AgregarUsuario(P_Usuario);
+        }
+
+        public List<Perfiles> ConsultarPerfiles(Perfiles P_Perfil)
+        {
+            return LogicNegocio.ConsultarPerfiles(P_Perfil);
+        }
+
+        public List<Usuarios> Consultar_Permisos_Usuarios(Usuarios P_usuario)
+        {
+            return LogicNegocio.Consultar_Permisos_Usuarios(P_usuario);
+        }
+
+        public List<Usuarios> Consultar_Usuarios()
+        {
+            return LogicNegocio.Consultar_Usuarios();
+        }
+
+        public void DoWork()
+        {
+        }
+
+        public int EliminarPerfil(Perfiles P_Perfil)
+        {
+            return LogicNegocio.EliminarPerfil(P_Perfil);
+        }
+
+        public int EliminarUsuario(Usuarios P_usuario)
+        {
+            return LogicNegocio.EliminarUsuario(P_usuario); 
+        }
+
+        public int ModificarPerfil(Perfiles P_Perfil)
+        {
+            return LogicNegocio.ModificarPerfil(P_Perfil);
+           
+        }
+
+        public int ModificarUsuario(Usuarios P_Usuario)
+        {
+            return LogicNegocio.ModificarUsuario(P_Usuario);
+        }
+
+        public bool VerificarUsuario(Usuarios P_usuario)
+        {
+            return LogicNegocio.VerificarUsuario(P_usuario);
+        }
+    }
+}
